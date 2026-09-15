9383cb28043efc843fa3c03472ea9052a66b039f
tmanfx1 <tmanfx1@gmail.com>
2026-09-15 23:11:13 +0200

    banner image bug fixed

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -81,9 +81,9 @@ const Home = () => {
     </div>
   </div>
 
-  <section className="hero">
+  {/* <section className="hero">
     <div className="hero__media">
-      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZ3hBIghhqNVz4ULvQE0s2xbGox9h7gbgNXC9Od3p0W22wAJfpYbi8myfr&s=10" alt="cover photo" />
+      <img src="https://boutique-homes-prod.sfo3.cdn.digitaloceanspaces.com/properties/4165/008-villa-camps-bay_villa_cape-town-south-africa_exterior-1753146831769.jpg" alt="cover photo" />
     </div>
     <div className="hero__media-fallback"></div>
 
@@ -91,7 +91,22 @@ const Home = () => {
       <h1 className="hero__title">Not sure where to go? Perfect.</h1>
       <button className="hero__cta">Get inspired</button>
     </div>
-  </section>
+  </section> */}
+
+  <section className="hero">
+  <div className="hero__media">
+    <img
+      src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZ3hBIghhqNVz4ULvQE0s2xbGox9h7gbgNXC9Od3p0W22wAJfpYbi8myfr&s=10"
+      alt="cover photo"
+    />
+  </div>
+  <div className="hero__media-fallback"></div>
+
+  <div className="hero__content">
+    <h1 className="hero__title">Not sure where to go? Perfect.</h1>
+    <button className="hero__cta">Get inspired</button>
+  </div>
+</section>
 
   <main>
     <section className="wrap section">
